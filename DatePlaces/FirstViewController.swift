e98065a80af151f35e473e6b12dd164778f8dd81
Paul O'Connor <jamespauloconnor@gmail.com>
2015-08-18 18:01:53 -0400

    Initial Commit

---
+++ DatePlaces/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DatePlaces
+//
+//  Created by Paul O'Connor on 8/18/15.
+//  Copyright (c) 2015 OCApps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
